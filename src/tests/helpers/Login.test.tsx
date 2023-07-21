6ca3debc4cc531e0a39e805f9258d522fdaf1a69
Danilo Bellini <bellini.daan@gmail.com>
2023-07-21 18:01:25 -0300

    Requisito 4 ok

--- src/tests/helpers/Login.test.tsx
+++ src/tests/helpers/Login.test.tsx
@@ -12,18 +12,18 @@ describe('Página de Login', () => {
     expect(loginBtn).toBeInTheDocument();
     expect(loginBtn).toBeDisabled();
   });
-  //   test('2 - Verifica o redirecionamento do botão Entrar e se ele está desativado na renderização', async () => {
-  //     renderWithRouterAndRedux(<App />);
-  //     const loginBtn = screen.getByRole('button', { name: 'Entrar' });
-  //     const loginInput = screen.getByPlaceholderText('Login');
-  //     const passwordInput = screen.getByPlaceholderText('Senha');
+  test('2 - Verifica o redirecionamento do botão Entrar e se ele está desativado na renderização', async () => {
+    renderWithRouterAndRedux(<App />);
+    const loginBtn = screen.getByRole('button', { name: 'Entrar' });
+    const loginInput = screen.getByPlaceholderText('Login');
+    const passwordInput = screen.getByPlaceholderText('Senha');
 
-//     expect(loginBtn).toBeInTheDocument();
-//     expect(loginBtn).toBeDisabled();
-//     await userEvent.type(loginInput, 'email@email.com');
-//     await userEvent.type(passwordInput, '1234567');
-//     expect(loginBtn).not.toBeDisabled();
-//     await userEvent.click(loginBtn);
-//     expect(window.location.pathname).toBe('/carteira');
-//   });
+    expect(loginBtn).toBeInTheDocument();
+    expect(loginBtn).toBeDisabled();
+    await userEvent.type(loginInput, 'email@email.com');
+    await userEvent.type(passwordInput, '1234567');
+    expect(loginBtn).not.toBeDisabled();
+    await userEvent.click(loginBtn);
+    expect(screen.getByRole('heading', { name: 'email@email.com' })).toBeInTheDocument();
+  });
 });

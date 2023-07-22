14cac94fb5a7a7a5e0d4159eefeecdcb3120a106
Danilo Bellini <bellini.daan@gmail.com>
2023-07-22 01:08:20 -0300

    Requisito 8

--- src/tests/helpers/WalletForm.test.tsx
+++ src/tests/helpers/WalletForm.test.tsx
@@ -1,13 +1,26 @@
-import { screen, waitFor } from '@testing-library/dom';
+import { screen } from '@testing-library/dom';
 import userEvent from '@testing-library/user-event';
 import { vi } from 'vitest';
 import { renderWithRouterAndRedux } from './renderWith';
 import App from '../../App';
 import mockData from './mockData';
-import mockFetch from './mocks/mockFetch';
+import Wallet from '../../pages/Wallet';
+import { ReduxState } from '../../types';
 
 const userMail = 'email@email.com';
 const userPassword = '1234567';
+
+const REDUX_STATE: ReduxState = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: [],
+    editor: false,
+    idToEdit: 0,
+  },
+};
 
 const loginSimulator = async () => {
   const loginBtn = screen.getByRole('button', { name: 'Entrar' });
@@ -35,53 +48,19 @@ const LOGIN_STATE = {
   },
 };
 
-const CURRENCIES = [
-  'USD', 'CAD', 'GBP', 'ARS', 'BTC', 'LTC', 'EUR',
-  'JPY', 'CHF', 'AUD', 'CNY', 'ILS', 'ETH', 'XRP', 'DOGE',
-];
-
 const REDUX_EXPENSE = {
-  user: {
-    email: '',
-  },
-  wallet: {
-    currencies: [],
-    expenses: [
-      {
-        id: 0,
-        value: '1500',
-        currency: 'CAD',
-        method: 'Cartão de crédito',
-        tag: 'Lazer',
-        description: 'Gasto de teste',
-        exchangeRates: mockData,
-      },
-      {
-        id: 1,
-        value: '1501',
-        currency: 'CAD',
-        method: 'Cartão de débito',
-        tag: 'Lazer',
-        description: 'Gasto',
-        exchangeRates: mockData,
-      },
-    ],
-    editor: false,
-    idToEdit: 0,
-  },
+  id: 0,
+  value: '10000',
+  description: 'Kinder Ovo',
+  currency: 'USD',
+  method: 'Cartão de crédito',
+  tag: 'Alimentação',
+  exchangeRates: mockData,
 };
 
 describe('Página /carteira', () => {
-  beforeEach(() => {
-    vi.spyOn(global, 'fetch').mockImplementation(mockFetch as any);
-  });
-
-  afterEach(() => {
-    vi.clearAllMocks();
-  });
-
-  test.skip('1 - Verifica se os inputs de valor e descrição estão vazios', async () => {
-    renderWithRouterAndRedux(<App />, { initialEntries: ['/carteira'] });
+  test('1 - Verifica se os inputs de valor e descrição estão vazios', async () => {
+    renderWithRouterAndRedux(<Wallet />);
 
     const valueInput = screen.getByPlaceholderText('Valor da despesa');
     const descriptionInput = screen.getByPlaceholderText('Descrição da despesa');
@@ -90,7 +69,7 @@ describe('Página /carteira', () => {
     expect(descriptionInput).toHaveTextContent('');
   });
 
-  test.skip('2 - Verifica se, após o login, o estado inicial consta atualizado', async () => {
+  test('2 - Verifica se, após o login, o estado inicial consta atualizado', async () => {
     const { store } = renderWithRouterAndRedux(<App />);
     // Simula login
     await loginSimulator();
@@ -98,12 +77,11 @@ describe('Página /carteira', () => {
   });
 
   test('3 - Verifica se, após adicionar uma despesa, consta atualizada no estado', async () => {
-    const { store } = renderWithRouterAndRedux(<App />, { initialEntries: ['/carteira'] });
-
-    vi.mock('./mocks/mockFetch');
-    (mockFetch as any).mockResolvedValue({
-      json: async () => ({ currencies: CURRENCIES }),
+    global.fetch = vi.fn().mockResolvedValue({
+      json: async () => (mockData),
     });
+
+    const { store } = renderWithRouterAndRedux(<Wallet />, { initialState: REDUX_STATE });
 
     expect(global.fetch).toHaveBeenCalled();
 
@@ -114,28 +92,12 @@ describe('Página /carteira', () => {
     const tag = screen.getByTestId('tag-input');
     const expenseBtn = screen.getByRole('button', { name: 'Adicionar despesa' });
 
-    expect(valueInput).toHaveTextContent('');
-    expect(descriptionInput).toHaveTextContent('');
-    await waitFor(() => {
-      expect(currency).toHaveValue('USD');
-    }, { timeout: 5000 });
-    expect(currency).toHaveValue('USD');
-    expect(method).toHaveTextContent('Dinheiro');
-    expect(tag).toHaveTextContent('Alimentação');
-    expect(expenseBtn).toBeInTheDocument();
-
-    await userEvent.type(valueInput, '1500');
-    await userEvent.type(descriptionInput, 'Gasto de teste');
-    await userEvent.selectOptions(currency, 'CAD');
+    await userEvent.type(valueInput, '10000');
+    await userEvent.type(descriptionInput, 'Kinder Ovo');
+    await userEvent.selectOptions(currency, 'USD');
     await userEvent.selectOptions(method, 'Cartão de crédito');
-    await userEvent.selectOptions(tag, 'Lazer');
+    await userEvent.selectOptions(tag, 'Alimentação');
     await userEvent.click(expenseBtn);
-    await userEvent.type(valueInput, '1501');
-    await userEvent.type(descriptionInput, 'Gasto');
-    await userEvent.selectOptions(currency, 'CAD');
-    await userEvent.selectOptions(method, 'Cartão de débito');
-    await userEvent.selectOptions(tag, 'Lazer');
-    await userEvent.click(expenseBtn);
-    expect(store.getState()).toMatchObject(REDUX_EXPENSE);
+    expect(store.getState().wallet.expenses[0]).toMatchObject(REDUX_EXPENSE);
   });
 });
